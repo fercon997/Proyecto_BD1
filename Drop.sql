eaba78b8f40959040fc0945f26cec160c23190f7
fercon997 <fercon997@gmail.com>
2018-02-18 11:35:39 -0400

    Archivo drop de las primeras tablas

---
+++ Drop.sql
@@ -0,0 +1,17 @@
+\connect proyecto
+
+DROP TABLE Parentesco_nino;
+DROP TABLE Parentesco_Padre;
+DROP TABLE Nino;
+DROP TABLE Horario_Act_Guarderia;
+DROP TABLE Act_Guarderia;
+DROP TABLE Actividad;
+DROP TABLE Autorizado;
+DROP TABLE Representante;
+DROP TABLE Experiencia;
+DROP TABLE Personal;
+DROP TABLE Guarderia;
+DROP TABLE Lugar;
+
+\connect postgres
+DROP DATABASE Proyecto;
