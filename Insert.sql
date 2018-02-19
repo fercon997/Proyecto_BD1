a49ff001d84b5a122c8cb962471adaa5b50110c1
fercon997 <fercon997@gmail.com>
2018-02-19 18:57:30 -0400

    Todas las guarderias

--- Insert.sql
+++ Insert.sql
@@ -24,4 +24,8 @@ insert into Lugar_4 values(20, 'Calle JM Vargas', 'Calle' ,19);
 insert into Lugar_4 values(21, 'Castillo San Carlos de Barromeo', 'Casa', 20);
 
 --Guarderias
-insert into Guarderia_4 values ('J29875441-5',1200, 500, 400, 1400, 250, '08:00', '18:00', 8);
+insert into Guarderia_4 values ('J298754541-5',1200, 500, 400, 1400, 250, '08:00', '18:00', 8);
+insert into Guarderia_4 values ('J764219187-2', 1250, 450, 500, 1500, 285, '08:30', '18:00', 10);
+insert into Guarderia_4 values ('J128487902-9', 1400, 500,  500, 1500, 400, '08:00', '17:00', 14);
+insert into Guarderia_4 values ('J516782778-4', 1100, 400, 400, 1200, 200, '07:00', '19:00', 17);
+insert into Guarderia_4 values ('J279154667-2', 1200, 500, 400, 1400, 250, '08:00', '18:00', 21);  
