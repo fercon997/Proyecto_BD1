1f7705f6fd3d9a132296c0a43fb4b2e40df9da95
Ignacio Paradisi <ignaciotfw@gmail.com>
2018-02-21 00:56:20 -0400

    Se hizo el select de los datos de las guarderías (sin las tarifas) incluidos los lugares

---
+++ Guarderia/src/Model/Lugar.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class Lugar {
+    private String casa;
+    private String calle;
+    private String municipio;
+    private String ciudad;
+    private String estado;
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
