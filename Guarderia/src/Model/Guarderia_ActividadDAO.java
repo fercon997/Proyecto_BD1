07153d6f764768a6f0befd632f1ebea96eab8b78
gabrielbaron16 <32284026+gabrielbaron16@users.noreply.github.com>
2018-02-22 00:09:02 -0400

    Add files via upload

--- Guarderia/src/Model/Guarderia_ActividadDAO.java
+++ Guarderia/src/Model/Guarderia_ActividadDAO.java
@@ -5,37 +5,11 @@
  */
 package Model;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 /**
  *
  * @author gabrielbaron
  */
-public class Guarderia_ActividadDAO {
-    DBConnection con = new DBConnection(); 
-    
-    public ArrayList<String> getactividades(String rif){
-        Connection connection = con.connectToPostgres();
-        ArrayList actividades = new ArrayList();
-        String sql = "SELECT Actividad.Nombre FROM Actividad_4 Actividad, Act_Guarderia_4 Act_guard "
-        + " WHERE Actividad.Codigo = Act_guard.Cod_actividad AND Act_guard.RIF_guarderia = '" + rif + "';";
-        try {
-            Statement st;
-            st = connection.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            while(rs.next()) {
-                actividades.add(rs.getString(1));
-            }
-            rs.close();
-            st.close();
-            connection.close();
-        } catch(SQLException e) {
-            System.out.println("Error");
-            e.getStackTrace();
-        }
-        return actividades;
-    }
+public interface Guarderia_ActividadDAO {
+    public ArrayList<Guarderia_Actividad> getactividades();
 }
